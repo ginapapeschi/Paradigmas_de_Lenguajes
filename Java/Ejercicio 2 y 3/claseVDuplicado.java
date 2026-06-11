ef283d20c7f1a770edcac3d557c8f5af562c5a7b
ginapapeschi <166958436+ginapapeschi@users.noreply.github.com>
2026-06-11 14:55:27 -0300

    Delete Java/Ejercicio 2 y 3 directory

--- Java/Ejercicio 2 y 3/claseVDuplicado.java
+++
@@ -1,5 +0,0 @@
-public class ViajeroDuplicadoException extends Exception{
-    public ViajeroDuplicadoException(String mensaje){
-        super(mensaje);
-    }
-}
--- Java/Ejercicio 2 y 3/claseVNoEncontrado.java
+++
@@ -1,5 +0,0 @@
-public class ViajeroNoEncontradoException extends Exception{
-    public ViajeroNoEncontradoException(String mensaje){
-        super(mensaje);
-    }
-}
